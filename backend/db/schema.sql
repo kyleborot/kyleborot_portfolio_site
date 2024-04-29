32b362793ceec0998b6de230a058b6680f11221e
Kyle Borot <kyleyabraborot@gmail.com>
2024-04-29 08:17:38 -0400

    created db schema and tables

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,2 +1,24 @@
 -- Here is the diagram for the db. WIP 4/28/24
 -- https://app.eraser.io/workspace/UGWcsZHI5LcDLqLktR72?origin=share
+CREATE DATABASE user_authentication;
+
+USE user_authentication
+GO
+
+CREATE SCHEMA LoginSchema
+GO
+CREATE TABLE LoginSchema.Users (
+	user_id int PRIMARY KEY,
+	first_name varchar(100),
+	last_name varchar(100),
+	email varchar(100)
+);
+
+CREATE TABLE LoginSchema.UserLogin (
+	login_id int PRIMARY KEY,
+	user_id int, --FK
+	login_name varchar(20),
+	hashed_password varchar(250), --stores the hashed password for verification
+	password_salt varchar(100), --stores the salt for verification
+	FOREIGN KEY (user_id) REFERENCES LoginSchema.Users(user_id) --1-to-1 relation
+);
