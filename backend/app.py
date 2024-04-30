1b573bf035161e96f8a4ab0f15bcf2afbb6a031e
kyleborot <119687434+kyleborot@users.noreply.github.com>
2024-04-30 06:57:20 -0400

    updated routing and structure setup

--- backend/app.py
+++ backend/app.py
@@ -1,28 +1,11 @@
 from flask import Flask
-import pyodbc
-from config.config import SERVER_NAME, DATABASE_NAME
+from routes.main_routes import main_routes
+from config.config import conn
 
 
 app = Flask(__name__)
 
-#SQL config
-server = SERVER_NAME
-database = DATABASE_NAME
-driver = '{ODBC Driver 17 for SQL Server}'
-
-conn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes;')
-
-@app.route("/")
-def hello():
-  return "Hello World!"
-
-@app.route("/test")
-def test():
-    cursor=conn.cursor()
-    cursor.execute("SELECT * FROM LoginSchema.Users") #SQL Connection Test
-    data = cursor.fetchall()
-    cursor.close()
-    return str(data)
+app.register_blueprint(main_routes)
 
 if __name__ == "__main__":
   app.run(debug=True)
---
+++ backend/config/user_registration.py
@@ -0,0 +1,5 @@
+from flask import Flask, render_template
+
+def user_registration():
+    
+    return
