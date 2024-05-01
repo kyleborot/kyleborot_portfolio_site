7e5df67bee614baae0e60ee1792fef0e49b329ce
Kyle Borot <kyleyabraborot@gmail.com>
2024-05-01 06:51:14 -0400

    updated config_example

--- backend/config/config_example.py
+++ backend/config/config_example.py
@@ -3,5 +3,6 @@ import pyodbc
 SERVER_NAME = "[INSERT SERVER CONFIG HERE]"
 DATABASE_NAME = "user_authentication"
 DRIVER = '{ODBC Driver 17 for SQL Server}'
+SECRET_KEY = ''
 
 conn = pyodbc.connect('DRIVER='+DRIVER+';SERVER='+SERVER_NAME+';DATABASE='+DATABASE_NAME+';Trusted_Connection=yes;')
