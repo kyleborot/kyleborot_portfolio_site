1af53c29a3879825c5abd08321541486c61a09c1
kyleborot <119687434+kyleborot@users.noreply.github.com>
2024-05-05 21:24:36 -0400

    changed user deletion method

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -11,7 +11,8 @@ CREATE TABLE LoginSchema.Users (
 	user_id int PRIMARY KEY,
 	first_name varchar(100),
 	last_name varchar(100),
-	email varchar(100)
+	email varchar(100),
+	isEmpty BIT DEFAULT 1
 );
 
 CREATE TABLE LoginSchema.UserLogin (
@@ -20,5 +21,6 @@ CREATE TABLE LoginSchema.UserLogin (
 	login_name varchar(20),
 	hashed_password varchar(250), --stores the hashed password for verification
 	password_salt varchar(100), --stores the salt for verification
+	isEmpty BIT DEFAULT 1,
 	FOREIGN KEY (user_id) REFERENCES LoginSchema.Users(user_id) --1-to-1 relation
 );
--- backend/login/user_authentication.py
+++ backend/login/user_authentication.py
@@ -4,14 +4,14 @@ from .password_hashing import password_hashing
 
 def get_max_user_id():
     cursor = conn.cursor()
-    cursor.execute("SELECT MAX(user_id) FROM LoginSchema.Users")
+    cursor.execute("SELECT MAX(user_id) FROM LoginSchema.Users WHERE isEmpty = 1")
     max_user_id = cursor.fetchone()[0]
     cursor.close()
     return max_user_id if max_user_id else 0
 
 def get_max_login_id():
     cursor = conn.cursor()
-    cursor.execute("SELECT MAX(login_id) FROM LoginSchema.UserLogin")
+    cursor.execute("SELECT MAX(login_id) FROM LoginSchema.UserLogin WHERE isEmpty = 1")
     max_login_id = cursor.fetchone()[0]
     cursor.close()
     return max_login_id if max_login_id else 0
@@ -21,9 +21,9 @@ login_id = get_max_login_id() + 1
 
 def login_user(username, password):
     cursor = conn.cursor()
-    cursor.execute("SELECT hashed_password, password_salt, login_name FROM LoginSchema.UserLogin WHERE login_name = ?", (username))
+    cursor.execute("SELECT hashed_password, password_salt, login_name, isEmpty FROM LoginSchema.UserLogin WHERE login_name = ?", (username))
     result = cursor.fetchone()
-    if result:
+    if result and result[3] == 1:
         stored_password = result[0]
         stored_salt = result[1]
         entered_password_hashed, _ = password_hashing(password, stored_salt)
@@ -45,20 +45,25 @@ def update_username(new_username, confirm_username, password, email):
     cursor.execute("SELECT user_id FROM LoginSchema.Users WHERE email = ?", (email))
     user_id = cursor.fetchone()
     if user_id:
-        cursor.execute("SELECT hashed_password, password_salt, login_id FROM LoginSchema.UserLogin WHERE login_id = ?", (user_id))
-        result = cursor.fetchone()
-        if result:
-            stored_password = result[0]
-            stored_salt = result[1]
-            entered_password_hashed, _ = password_hashing(password, stored_salt)
-            if stored_password == entered_password_hashed and user_id[0] == result[2]:
-                if new_username == confirm_username:
-                    cursor.execute("UPDATE LoginSchema.UserLogin SET login_name = ? WHERE user_id = ?", (new_username, user_id[0]))
-                    conn.commit()
-                    cursor.close()
-                    return "User updated successfully"
+        cursor.execute("SELECT isEmpty FROM LoginSchema.Users WHERE user_id = ?", (user_id[0],))
+        isEmpty = cursor.fetchone()
+        if isEmpty[0] == 1:
+            cursor.execute("SELECT hashed_password, password_salt, login_id FROM LoginSchema.UserLogin WHERE login_id = ?", (user_id[0]))
+            result = cursor.fetchone()
+            if result:
+                stored_password = result[0]
+                stored_salt = result[1]
+                entered_password_hashed, _ = password_hashing(password, stored_salt)
+                if stored_password == entered_password_hashed and user_id[0] == result[2]:
+                    if new_username == confirm_username:
+                        cursor.execute("UPDATE LoginSchema.UserLogin SET login_name = ? WHERE user_id = ?", (new_username, user_id[0]))
+                        conn.commit()
+                        cursor.close()
+                        return "User updated successfully"
+                else:
+                    return "Either your username confirmation did not match, or the password you have entered is incorrect."
             else:
-                return "Either your username confirmation did not match, or the password you have entered is incorrect."
+                return "user does not exist"
     return "There has been a problem, please try again later."
 
 def update_user_password(username, email, new_password, confirm_password):
@@ -67,7 +72,9 @@ def update_user_password(username, email, new_password, confirm_password):
     id = cursor.fetchone()
     cursor.execute("SELECT email FROM LoginSchema.Users WHERE user_id = ?", (id[0],))
     validate_email = cursor.fetchone()
-    if email == validate_email[0] and new_password == confirm_password:
+    cursor.execute("SELECT isEmpty FROM LoginSchema.Users WHERE user_id = ?", (id[1],))
+    isEmpty = cursor.fetchone()
+    if email == validate_email[0] and new_password == confirm_password and isEmpty[0] == 1:
         password, salt = password_hashing(new_password)
         cursor.execute("UPDATE LoginSchema.UserLogin SET hashed_password = ?, password_salt = ? WHERE login_id = ?", (password, salt, id[0]))
         conn.commit()
@@ -82,7 +89,9 @@ def delete_user(username, password, email, delete_form):
     if result_1:
         cursor.execute("SELECT email FROM LoginSchema.Users WHERE user_id = ?", (result_1[0],))
         validate_email = cursor.fetchone()
-        if email == validate_email[0]:
+        cursor.execute("SELECT isEmpty FROM LoginSchema.Users WHERE user_id = ?", (result_1[0],))
+        isEmpty = cursor.fetchone()
+        if email == validate_email[0] and isEmpty[0] == 1:
             cursor.execute("SELECT hashed_password, password_salt, login_id FROM LoginSchema.UserLogin WHERE login_id = ?", (result_1[0],))
             pw_result = cursor.fetchone()
             if pw_result:
@@ -90,8 +99,8 @@ def delete_user(username, password, email, delete_form):
                 stored_salt = pw_result[1]
                 entered_password_hashed, _ = password_hashing(password, stored_salt)
                 if stored_password == entered_password_hashed and delete_form == "delete":
-                    cursor.execute("DELETE FROM LoginSchema.UserLogin WHERE login_id = ?", (result_1[0],))
-                    cursor.execute("DELETE FROM LoginSchema.Users WHERE user_id = ?", (result_1[0],))
+                    cursor.execute("UPDATE LoginSchema.UserLogin SET isEmpty = 0 WHERE login_id = ?", (result_1[0],))
+                    cursor.execute("UPDATE LoginSchema.Users SET isEmpty = 0 WHERE user_id = ?", (result_1[0],))
                     conn.commit()
                     cursor.close()
                     return "user successfully deleted"
