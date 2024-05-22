5c0afcecc4145ad051e543d6424567f4899a1faf
Kyle Borot <kyleyabraborot@gmail.com>
2024-05-22 07:29:45 -0400

    ui changes

--- frontend/src/components/Home/About.tsx
+++ frontend/src/components/Home/About.tsx
@@ -2,13 +2,30 @@
 
 import React from 'react';
 import { Element } from 'react-scroll';
+import './HomeComponent.css'
 
 const About: React.FC = () => {
   return (
     <Element name="about">
-        <div>
-        <h2>About</h2>
-        {/* Add your About content here */}
+        <div className="container">
+            <div className="text-section">
+                <p>
+                    Hi, I'm Kyle Borot, a Mechanical Engineering and Computer Science student at Florida International University. 
+                    Over my college years, I've worked in various engineering fields, including civil, mechanical, and software engineering. 
+                    I've gained hands-on experience in industries like infrastructure, solar energy, defense, and nautical engineering. 
+                    This mix of experiences has given me a solid grasp of engineering principles and made me a flexible engineer ready to tackle any challenge.
+                </p>
+                <p>
+                    When I'm not immersed in my studies or projects, you'll find me cheering on the Miami Dolphins or trying out new foods. 
+                    I'm a huge American football fan and love exploring different cuisines. 
+                    Lately, I've also been diving into the world of financial markets, learning about trading and investment strategies. 
+                    These interests help me balance my busy schedule and keep me curious and motivated.
+                </p>
+            </div>
+            <div className="image-container">
+                {/* Future image can be included here */}
+                {/* <img src="/about_photo.png" alt="Kyle Borot"/> */}
+            </div>
         </div>
     </Element>
   );
--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -2,8 +2,8 @@
 
 import React from 'react';
 import { NavLink as RouterLink, useLocation, useNavigate } from 'react-router-dom';
-import { Link as ScrollLink } from 'react-scroll';
 import * as Scroll from 'react-scroll';
+import './HomeComponent.css'
 
 const Home: React.FC = () => {
   const path = useLocation().pathname;
@@ -22,14 +22,19 @@ const Home: React.FC = () => {
     };
 
   return (
-    <div>
-      <h1>Hello!</h1> 
-      <h1>I'm Kyle, </h1>
-      <h1>an Engineer</h1>
-      <img src="/home_photo.png" alt="profile photo"/>
-      <button>Learn what that means</button>
-      <button>Let's work together!</button>
-
+    <div className="container">
+      <div className="text-buttons">
+        <h1>Hello!</h1> 
+        <h1>I'm Kyle, </h1>
+        <h1>an engineer</h1>
+        <div className="buttons">
+          <button onClick={() => goToPageAndScroll("about")}>Learn what that means</button>
+          <button onClick={() => goToPageAndScroll("contact")}>Let's work together!</button>
+        </div>
+      </div>
+        <div className="image-container">
+          <img src="/home_photo.png" alt="profile photo"/>
+        </div>
     </div>
   );
 }
