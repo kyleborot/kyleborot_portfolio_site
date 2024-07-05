00336c5c17b185f77a75de72e34c5b153650d207
kyleborot <119687434+kyleborot@users.noreply.github.com>
2024-07-05 11:40:35 -0400

    api data updates

--- backend/api/project_page_api/projects.py
+++ backend/api/project_page_api/projects.py
@@ -16,7 +16,7 @@ projects to include
 11 project cards total
 """
 projects = [
-    {
+        {
         "id": 1,
         "name": "Portfolio Site",
         "shortDescription": "The very site you are on right now! I built this full-stack portfolio website from scratch.",
@@ -27,35 +27,35 @@ projects = [
         "createdDate": "2024-01-01",
         "createdLocation": "Miami, FL",
         "techUsed": "Python, Flask, React, SQL"
-    },
+        },
         {
         "id": 2,
-        "name": "Miter Assembly Script",
-        "shortDescription": "The very site you are on right now! I built this full-stack portfolio website from scratch.",
+        "name": "Automated Material Quantification for Fabrication",
+        "shortDescription": "A project using AlibreX to dynamically take the measurements of 3D models.",
         "longDescription":"placeholder",
-        "isDemo": True,
+        "isDemo": False,
         "projectURL":"http://example.com/project1",
         "photoURL": "http://example.com/photo1.jpg",
-        "createdDate": "2024-01-01",
-        "createdLocation": "Miami, FL",
-        "techUsed": "Python, Flask, React, SQL"
-    },
-    {
+        "createdDate": "2024-07-19",
+        "createdLocation": "Ft. Lauderdale, FL",
+        "techUsed": "Python, AlibreX API, Data Structures, Algorithms"
+        },
+        {
         "id": 3,
-        "name": "Textbook AI",
-        "shortDescription": "The very site you are on right now! I built this full-stack portfolio website from scratch.",
+        "name": "Combined LSTM Modeling and Sentiment Analysis",
+        "shortDescription": "Using an LSTM model and LLM model to forecast market movement with sentiment analysis",
         "longDescription":"placeholder",
         "isDemo": True,
         "projectURL":"http://example.com/project1",
         "photoURL": "http://example.com/photo1.jpg",
         "createdDate": "2024-01-01",
         "createdLocation": "Miami, FL",
-        "techUsed": "Python, Flask, React, SQL"
-    },
-    {
+        "techUsed": "Python, LLaMA3, LSTM models"
+        },
+        {
         "id": 4,
-        "name": "Portfolio Site",
-        "shortDescription": "The very site you are on right now! I built this full-stack portfolio website from scratch.",
+        "name": "Portfolio Site User Login",
+        "shortDescription": "User authentication system made for this website",
         "longDescription":"placeholder",
         "isDemo": True,
         "projectURL":"http://example.com/project1",
@@ -63,90 +63,90 @@ projects = [
         "createdDate": "2024-01-01",
         "createdLocation": "Miami, FL",
         "techUsed": "Python, Flask, React, SQL"
-    },
-    {
+        },
+        {
         "id": 5,
-        "name": "Portfolio Site",
-        "shortDescription": "The very site you are on right now! I built this full-stack portfolio website from scratch.",
+        "name": "Miter Assembly Script",
+        "shortDescription": "A script that automates the creation of Miter Elbow assemblies.",
         "longDescription":"placeholder",
-        "isDemo": True,
+        "isDemo": False,
         "projectURL":"http://example.com/project1",
         "photoURL": "http://example.com/photo1.jpg",
-        "createdDate": "2024-01-01",
-        "createdLocation": "Miami, FL",
-        "techUsed": "Python, Flask, React, SQL"
-    },
-    {
+        "createdDate": "2024-04-08",
+        "createdLocation": "Ft. Lauderdale, FL",
+        "techUsed": "Python, AlibreScripts API"
+        },
+        {
         "id": 6,
-        "name": "Portfolio Site",
-        "shortDescription": "The very site you are on right now! I built this full-stack portfolio website from scratch.",
+        "name": "Iron EagleX Pokedex",
+        "shortDescription": "A MiniDex built with React and data from PokeAPI. Deployed on AWS.",
         "longDescription":"placeholder",
         "isDemo": True,
         "projectURL":"http://example.com/project1",
         "photoURL": "http://example.com/photo1.jpg",
-        "createdDate": "2024-01-01",
+        "createdDate": "2024-02-20",
         "createdLocation": "Miami, FL",
-        "techUsed": "Python, Flask, React, SQL"
-    },
-    {
+        "techUsed": "Vite React, TypeScript, AWS"
+        },
+        {
         "id": 7,
-        "name": "Portfolio Site",
-        "shortDescription": "The very site you are on right now! I built this full-stack portfolio website from scratch.",
+        "name": "Waste Reduction Autonomous Project - Senior Design",
+        "shortDescription": "Senior Design Project focused on a trash pickup robot",
+        "longDescription":"placeholder",
+        "isDemo": False,
+        "projectURL":"http://example.com/project1",
+        "photoURL": "http://example.com/photo1.jpg",
+        "createdDate": "2024-12-01",
+        "createdLocation": "Miami, FL",
+        "techUsed": "Solidworks, Project Management, Python"
+        },
+        {
+        "id": 8,
+        "name": "Exowatt Internship",
+        "shortDescription": "Culmination of Summer 2023 internship experience at Exowatt",
+        "longDescription":"placeholder",
+        "isDemo": False,
+        "projectURL":"http://example.com/project1",
+        "photoURL": "http://example.com/photo1.jpg",
+        "createdDate": "2023-08-18",
+        "createdLocation": "Miami, FL",
+        "techUsed": "Solidworks, Python, Arduino (C language)"
+        },
+        {
+        "id": 9,
+        "name": "UniFace (eMerge 2023)",
+        "shortDescription": "4th-place hackathon project focused on facial recognition",
+        "longDescription":"placeholder",
+        "isDemo": False,
+        "projectURL":"http://example.com/project1",
+        "photoURL": "http://example.com/photo1.jpg",
+        "createdDate": "2023-04-16",
+        "createdLocation": "Miami, FL",
+        "techUsed": "Python, EfficientNetB0"
+        },
+        {
+        "id": 10,
+        "name": "L'SPACE Mission Concept Academy",
+        "shortDescription": "L'SPACE Project focused on rover design for lunar regolith",
+        "longDescription":"placeholder",
+        "isDemo": False,
+        "projectURL":"http://example.com/project1",
+        "photoURL": "http://example.com/photo1.jpg",
+        "createdDate": "2022-04-25",
+        "createdLocation": "Miami, FL",
+        "techUsed": "Solidworks, Project Management"
+        },
+        {
+        "id": 11,
+        "name": "CAD Portfolio",
+        "shortDescription": "A collection of 2D and 3D CAD Designs",
         "longDescription":"placeholder",
         "isDemo": True,
         "projectURL":"http://example.com/project1",
         "photoURL": "http://example.com/photo1.jpg",
         "createdDate": "2024-01-01",
         "createdLocation": "Miami, FL",
-        "techUsed": "Python, Flask, React, SQL"
-    },
-    {
-        "id": 1,
-        "name": "Portfolio Site",
-        "shortDescription": "The very site you are on right now! I built this full-stack portfolio website from scratch.",
-        "longDescription":"placeholder",
-        "isDemo": True,
-        "projectURL":"http://example.com/project1",
-        "photoURL": "http://example.com/photo1.jpg",
-        "createdDate": "2024-01-01",
-        "createdLocation": "Miami, FL",
-        "techUsed": "Python, Flask, React, SQL"
-    },
-    {
-        "id": 1,
-        "name": "Portfolio Site",
-        "shortDescription": "The very site you are on right now! I built this full-stack portfolio website from scratch.",
-        "longDescription":"placeholder",
-        "isDemo": True,
-        "projectURL":"http://example.com/project1",
-        "photoURL": "http://example.com/photo1.jpg",
-        "createdDate": "2024-01-01",
-        "createdLocation": "Miami, FL",
-        "techUsed": "Python, Flask, React, SQL"
-    },
-    {
-        "id": 1,
-        "name": "Portfolio Site",
-        "shortDescription": "The very site you are on right now! I built this full-stack portfolio website from scratch.",
-        "longDescription":"placeholder",
-        "isDemo": True,
-        "projectURL":"http://example.com/project1",
-        "photoURL": "http://example.com/photo1.jpg",
-        "createdDate": "2024-01-01",
-        "createdLocation": "Miami, FL",
-        "techUsed": "Python, Flask, React, SQL"
-    },
-    {
-        "id": 1,
-        "name": "Portfolio Site",
-        "shortDescription": "The very site you are on right now! I built this full-stack portfolio website from scratch.",
-        "longDescription":"placeholder",
-        "isDemo": True,
-        "projectURL":"http://example.com/project1",
-        "photoURL": "http://example.com/photo1.jpg",
-        "createdDate": "2024-01-01",
-        "createdLocation": "Miami, FL",
-        "techUsed": "Python, Flask, React, SQL"
-    },
+        "techUsed": "AutoCAD, Solidworks, Alibre Design, Fusion360"
+        },
 
 ]
